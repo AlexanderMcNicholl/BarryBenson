da6ff929836355974511d31cb33ef4342af856c3
AlexanderMcNicholl <alexander.mcnicholl@hotmail.com>
2018-10-15 12:00:10 +1000

    Fixed Errors

--- Bot.py
+++ Bot.py
@@ -33,6 +33,17 @@ selections = [
 	'maybe I don\'t care'
 ]
 
+@client.event
+async def on_message(message):
+	with open('members.json', 'r') as f:
+		users = json.load(f)
+	await update_json(users, message.author)
+	await add_exp(users, message.author, 5)
+	await update_level(users, message.author, message.channel)
+	with open('members.json', 'w') as f:
+		json.dump(users, f)
+	client.process_message(message)
+
 async def get_logs_from(channel):
 	f = open("tdata.txt", "a")
 	async for m in client.logs_from(channel):
@@ -222,16 +233,6 @@ async def magic_eight_ball(ctx, msg):
 @client.command(pass_context=True, name='pm')
 async def change_playing_message(ctx, *msg):
 	await client.change_presence(game=discord.Game(name=" ".join(msg)))
-
-@client.event
-async def on_message(message):
-	with open('members.json', 'r') as f:
-		users = json.load(f)
-	await update_json(users, message.author)
-	await add_exp(users, message.author, 5)
-	await update_level(users, message.author, message.channel)
-	with open('members.json', 'w') as f:
-		json.dump(users, f)
 
 @client.event
 async def on_ready():
